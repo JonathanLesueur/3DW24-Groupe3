ac1692652a975ba12623dd1fc300581e21012b8a
Buralis <72046129+Buralis@users.noreply.github.com>
2020-12-20 16:41:34 +0100

    Ajout des ingrédients

--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingredients.component.scss']
 })
 export class IngredientsComponent implements OnInit {
-
+  public time: number = 0;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  changeTime(type: String): void{
+    switch(type){
+      case '+':
+        this.time++;
+      break;
+      case '-':
+        if(this.time>0){
+          this.time--;
+        }
+      break;
+    }
   }
 
 }
